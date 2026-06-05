ddef3fddf982cc9a824a60a11de3f9703b77b5de
Pulkit Choudhary <gpulkit744@gmail.com>
2026-06-05 22:26:00 +0530

    basic segregated

--- ApiGateway/server.ts
+++ ApiGateway/server.ts
@@ -11,21 +11,16 @@ import PaymentRoutes from "./Routes/PaymentRoutes";
 import helmet from "helmet";
 import { merchantClient } from "./GrpcRef/Grpc";
 import { JwtAuthMiddleware } from "./Middleware/jwtAuth";
-// import { METHODS } from "http";
 
 app.use(experes.urlencoded({ extended: true }));
 app.use(experes.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use("/webhook/razorpay", experes.raw({ type: "application/json" }));
-// app.use(
-//   cors({
-//     origin: process.env.FrontendURL,
-//     methods: ["POST", "GET", "PUT", "PATCH"],
-//     credentials: true,
-//   }),
-// );
+
 app.use("/api/v1", MerchantRoutes);
 app.use("/api/v2", JwtAuthMiddleware, PaymentRoutes);
 const PORT = process.env.PORT || 6283;
-app.listen(PORT, () => [console.log(`Server is listening on port ${PORT}`)]);
+app.listen(PORT, async () => {
+  [console.log(`Server is listening on port ${PORT}`)];
+});
